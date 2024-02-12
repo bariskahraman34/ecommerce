a50decffc8a8bcecfa5af8264116e4307da1d77f
Barış Kahraman <baris-kahra@hotmail.com>
2024-02-12 23:16:12 +0300

    tıklanan ürünü listelemek için oluşturulan html listProduct fonksiyonuna eklendi. Üründen alınan bilgi html'in title bölümüne yerleştirildi.

--- assets/js/productPage.js
+++ assets/js/productPage.js
@@ -13,50 +13,52 @@ async function fetchDummyJson(endpoint){
 
 async function listProduct(){
     const product = await fetchDummyJson(`products/${clickedProductId}`);
-      container.innerHTML = 
-        `
-        <div class="product-col-3">
-            <a href="product-page.html" class="product" data-productid=${product.id}>
-                <div class="image-container">
-                    ${product.discountPercentage ? `<b class="badge-sale">SALE</b>` : ""}
-                    <img src="${product.thumbnail}" alt="">
-                </div>
-                <div class="product-info-area">
-                    <div class="product-brand-description">
-                        <span class="brand">${product.brand}</span>
-                        <span class="name">${product.title}</span>
-                    </div>
-                    <div class="rating-container">
-                        <div class="rating-score">${product.rating}</div>
-                        <div class="ratings">
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="price-wrapper">
-                        ${product.discountPercentage ? `
-                        <b class="badge-old-price">$${(product.price / (1 - product.discountPercentage / 100)).toFixed(2)}</b>
-                        `:""}
-                        
-                        <b class="badge-new-price">$${product.price}</b>
-                    </div>
-                </div>
-            </a>
+    const title = document.querySelector('title');
+    title.textContent = `${product.brand} - ${product.title}`;
+    console.log(product);
+    container.innerHTML = 
+    `
+    <div class="product-container">
+        <div class="product-left-side-container">
+            <div class="big-image-container">
+                <img src="${product.images[0]}" alt="">
+            </div>
+            <div class="small-image-container">
+            </div>
         </div>
-        `;
+        <div class="product-right-side-container">
+            <span class="brand">${product.brand}</span>
+            <span class="name">${product.title}</span>
+            <p class="description">
+                ${product.description}
+            </p>
+            <div class="price-container">
+                <div class="current-price-container">
+                    <span class="badge-new-price">$${product.price}</span>
+                    <span class="discount-rate">${product.discountPercentage}%</span>
+                </div>
+                <span class="badge-old-price">$${(product.price / (1 - product.discountPercentage / 100)).toFixed(2)}</span>
+            </div>
+            <div class="add-card-container">
+                <div class="quantitiy-container">
+                    <a href="#" class="quantity-down">-</a>
+                    <strong>0</strong>
+                    <a href="#" class="quantity-up">+</a>
+                </div>
+                <button class="big-btn">
+                    <img src="assets/icons/basket-white.svg" alt="">
+                    Add To Cart</button>
+            </div>
+        </div>
+    </div>
+    `;
+    const imagesContainer = document.querySelector('.small-image-container');
+    for (const image of product.images) {
+        imagesContainer.innerHTML += 
+        `
+        <img src="${image}" alt="">
+        `
+    }
 }
 
 listProduct();
