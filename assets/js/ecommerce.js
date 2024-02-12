b6cfd25b80b1561de0b23ecff5ce11720f0381c8
Barış Kahraman <baris-kahra@hotmail.com>
2024-02-13 02:20:48 +0300

    tıklanan ürünün tutulduğu localstorage kaldırıldı. bunun yerine tıklanan elemana id parametresi verildi ve productpage.js dosyasında buna uygun işlemlerin yapılması için yönlendirildi.

--- assets/js/ecommerce.js
+++ assets/js/ecommerce.js
@@ -1,12 +1,6 @@
 const BASE_URL = "https://dummyjson.com";
 const productPage = document.querySelector('.product-page');
 
-let clickedProductStorage = JSON.parse(localStorage.getItem('clickedProduct')) || {product:0};
-
-function saveClickedProductToLocalStorage(){
-  return localStorage.setItem('clickedProduct',JSON.stringify(clickedProductStorage));
-}
-
 async function fetchDummyJson(endpoint){
     const response = await fetch(`${BASE_URL}/${endpoint}`);
     const items = await response.json();
@@ -16,68 +10,53 @@ async function fetchDummyJson(endpoint){
 async function listProducts(){
     const items = await fetchDummyJson("products?limit=100");
     const products = items.products;
-    console.log(products);
     for (const product of products) {
-      productPage.innerHTML += 
-        `
-        <div class="product-col-3">
-            <a href="product-page.html" class="product" data-productid=${product.id}>
-                <div class="image-container">
-                    ${product.discountPercentage ? `<b class="badge-sale">SALE</b>` : ""}
-                    <img src="${product.thumbnail}" alt="">
-                </div>
-                <div class="product-info-area">
-                    <div class="product-brand-description">
-                        <span class="brand">${product.brand}</span>
-                        <span class="name">${product.title}</span>
+        productPage.innerHTML += 
+            `
+            <div class="product-col-3">
+                <a href="product-page.html?id=${product.id}" class="product" data-productid=${product.id}>
+                    <div class="image-container">
+                        ${product.discountPercentage ? `<b class="badge-sale">SALE</b>` : ""}
+                        <img src="${product.thumbnail}" alt="">
                     </div>
-                    <div class="rating-container">
-                        <div class="rating-score">${product.rating}</div>
-                        <div class="ratings" data-productid=${product.id}>
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <div class="star">
-                                <i class="fa-regular fa-star"></i>
+                    <div class="product-info-area">
+                        <div class="product-brand-description">
+                            <span class="brand">${product.brand}</span>
+                            <span class="name">${product.title}</span>
+                        </div>
+                        <div class="rating-container">
+                            <div class="rating-score">${product.rating}</div>
+                            <div class="ratings" data-productid=${product.id}>
+                                <div class="star">
+                                    <i class="fa-regular fa-star"></i>
+                                </div>
+                                <div class="star">
+                                    <i class="fa-regular fa-star"></i>
+                                </div>
+                                <div class="star">
+                                    <i class="fa-regular fa-star"></i>
+                                </div>
+                                <div class="star">
+                                    <i class="fa-regular fa-star"></i>
+                                </div>
+                                <div class="star">
+                                    <i class="fa-regular fa-star"></i>
+                                </div>
                             </div>
                         </div>
+                        <div class="price-wrapper">
+                            ${product.discountPercentage ? `
+                            <b class="badge-old-price">$${(product.price / (1 - product.discountPercentage / 100)).toFixed(2)}</b>
+                            `:""}
+                            
+                            <b class="badge-new-price">$${product.price}</b>
+                        </div>
                     </div>
-                    <div class="price-wrapper">
-                        ${product.discountPercentage ? `
-                        <b class="badge-old-price">$${(product.price / (1 - product.discountPercentage / 100)).toFixed(2)}</b>
-                        `:""}
-                        
-                        <b class="badge-new-price">$${product.price}</b>
-                    </div>
-                </div>
-            </a>
-        </div>
-        `;
-        ratingProduct(product.rating,product.id);
+                </a>
+            </div>
+            `;
+            ratingProduct(product.rating,product.id);
     }
-    bindEvents(".product","click",clickedProduct);
-}
-
-function bindEvents(selector , eventType , cbFunction){
-    const selectedElements = document.querySelectorAll(selector)
-    for (const selectedElement of selectedElements) {
-        selectedElement.addEventListener(eventType,cbFunction);
-    }
-}
-
-function clickedProduct(){
-    console.log(this.dataset.productid);
-    clickedProductStorage.product = this.dataset.productid;
-    saveClickedProductToLocalStorage();
 }
 
 function ratingProduct(rating,productId){
