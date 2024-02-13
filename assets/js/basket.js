430eaf6629ee54d899d41eeed280e0a460c96ee6
Barış Kahraman <baris-kahra@hotmail.com>
2024-02-14 01:54:49 +0300

    Sepette ki ürünlerin sayısını sepet ikonunda göstermek için js çalışması yapıldı. Delete btn için bir dialog oluşturuldu ve işlevi yazıldı. window a event listener ekleyerek kullanıcı sepet görüntülenmesini açtığında sepet dışında bir yere tıkladığında sepetin kapanması için gerekli js kodu yazıldı.

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,8 +1,12 @@
 const basketBtn = document.querySelector('.basket-btn');
 const basketResult = document.querySelector('.basket-result');
+const basketQuantity = document.querySelector('.basket-quantity');
 
 let productsSaved = JSON.parse(localStorage.getItem('products')) || [];
 let basketResultInfos = [];
+let basketQuantityTotal = 0;
+
+basketQuantity.innerHTML = `${basketQuantityTotal}`
 
 basketBtn.addEventListener('click',calculateBasket);
 
@@ -37,57 +41,93 @@ function calculateBasket(){
                     );
                 }
             }
-        }
+    }
         
-        for (const basket of basketResultInfos) {
-            basketResult.innerHTML += 
-            `
-            <div class="cart-body" data-productid=${basket.id}>
-                <div class="cart-container">            
-                    <img src="${basket.image}" width="50px" height="50px">
-                    <div class="cart-infos">
-                        <div>
-                            <span class="thin-text">${basket.title}</span>
-                        </div>
-                        <div> 
-                            <span class="product-price thin-text">$${basket.productPrice}</span>
-                            <span class="thin-text">x</span>
-                            <span class="thin-text">${basket.quantity}</span>
-                            <span class="bold-text">$${basket.totalPrice}</span>
-                        </div>
+    for (const basket of basketResultInfos) {
+        basketResult.innerHTML += 
+        `
+        <div class="cart-body" data-productid=${basket.id}>
+            <div class="cart-container">            
+                <img src="${basket.image}" width="50px" height="50px">
+                <div class="cart-infos">
+                    <div>
+                        <span class="thin-text">${basket.title}</span>
+                    </div>
+                    <div> 
+                        <span class="product-price thin-text">$${basket.productPrice}</span>
+                        <span class="thin-text">x</span>
+                        <span class="thin-text">${basket.quantity}</span>
+                        <span class="bold-text">$${basket.totalPrice}</span>
                     </div>
                 </div>
-                <button class="delete-btn"><img class="del-icon" src="assets/icons/del-icon.svg"/></button>
             </div>
-            `;
-        }
-        bindDeleteBtns();
-        document.querySelector('.close-offcanvas-btn').addEventListener('click',closeCanvas);
+            <button class="delete-btn" onclick="deleteProduct(${basket.id})" ><img class="del-icon" src="assets/icons/del-icon.svg"/></button>
+        </div>
+        `;
     }
-    
-    function bindDeleteBtns(){
-        const deleteBtns = document.querySelectorAll('.delete-btn');
-    for (const btn of deleteBtns) {
-        btn.addEventListener('click',deleteProduct);
+    document.querySelector('.close-offcanvas-btn').addEventListener('click',closeCanvas);
+    calculateBasketQuantity();
+}
+
+function calculateBasketQuantity(){
+    let basketSaved = JSON.parse(localStorage.getItem('basket')) || [];
+    basketQuantityTotal = 0;
+    if(basketSaved.length > 0){
+        for (const basket of basketSaved) {
+            basketQuantityTotal += basket.quantity;
+        }
+        const basketQuantity = document.querySelector('.basket-quantity');
+        basketQuantity.innerHTML = basketQuantityTotal;
+    }else{
+        basketQuantity.innerHTML = "0"
     }
 }
 
-function deleteProduct(e){
-    const answer = confirm('Silmek istediğinize emin misiniz ?');
-    if(answer){
-        let basketSaved = JSON.parse(localStorage.getItem('basket')) || [];
-        let findProduct = basketSaved.findIndex(item => {
-            return item.id === Number(e.target.parentElement.parentElement.dataset.productid)
+function deleteProduct(id){
+    if(document.querySelector('.dialog')){
+        document.querySelector('.dialog').remove();
+    }
+    const modal = 
+    `
+    <dialog class="dialog">
+        <h3>Delete Product</h3>
+        <p>Are you sure you want to delete this product? This will remove the product and can’t be undone.
+        </p>
+        <div class="dialog-buttons">
+            <button id="no" class="dialog-btn">no,cancel</button>
+            <button id="yes" class="dialog-btn">yes,delete</button>
+        </div>
+    </dialog>
+    `;
+    const body = document.querySelector('body');
+    body.insertAdjacentHTML('beforeend',modal);
+    document.querySelector('.dialog').showModal();
+    const dialogBtns = document.querySelectorAll('.dialog-btn');
+    for (const btn of dialogBtns) {
+        btn.addEventListener('click',function(){
+            document.querySelector('.dialog').close();
+            if(btn.id == "yes"){
+                let basketSaved = JSON.parse(localStorage.getItem('basket')) || [];
+                let findProduct = basketSaved.findIndex(item => {
+                    return item.id === Number(id)
+                })
+                if(findProduct !== -1){
+                    basketSaved.splice(findProduct,1);
+                    localStorage.setItem('basket',JSON.stringify(basketSaved));
+                    return calculateBasket();
+                }
+            }
         })
-        if(findProduct !== -1){
-            basketSaved.splice(findProduct,1);
-            localStorage.setItem('basket',JSON.stringify(basketSaved));
-            console.log('Eleman Kaldırıldı');
-            return calculateBasket();
-        }
     }
 }
-
 function closeCanvas(){
     basketResult.classList.remove("show");
 }
+
+window.addEventListener('click',function(e){
+    if(!basketResult.contains(e.target) && !basketBtn.contains(e.target)){
+        basketResult.classList.remove('show');
+    }
+})
+
+calculateBasketQuantity();
