149c471ca070ff39d6adaf5a0d38ae00f3b95129
Barış Kahraman <baris-kahra@hotmail.com>
2024-02-19 18:30:39 +0300

    Ürünlerin bir anda yüklenmesini önlemek için bir fonksiyon yazıldı ve her scroll sonuna gelindiğinde 9 ürün yüklenebilmesi için dummyjson'ın skip parametresi kullanıldı.

--- assets/js/ecommerce.js
+++ assets/js/ecommerce.js
@@ -7,12 +7,90 @@ async function fetchDummyJson(endpoint){
     return items;
 }
 
-async function listProducts(){
-    const items = await fetchDummyJson("products?limit=100");
+let loading = false;
+let skip = 0; 
+
+window.addEventListener('scroll', function() {
+    if ((window.innerHeight + window.scrollY >= document.body.offsetHeight) && !loading) {
+      loading = true;
+      loadMoreProducts();
+    }
+  });
+
+// async function listProducts(){
+//     const items = await fetchDummyJson("products?limit=100");
+//     const products = items.products;
+//     localStorage.setItem('products',JSON.stringify(items.products));
+//     for (const product of products) {
+//         productPage.innerHTML += 
+//             `
+//             <div class="product-col-3">
+//                 <a href="product-page.html?id=${product.id}" class="product" data-productid=${product.id}>
+//                     <div class="image-container">
+//                         ${product.discountPercentage ? `<b class="badge-sale">SALE</b>` : ""}
+//                         <img src="${product.thumbnail}" alt="">
+//                     </div>
+//                     <div class="product-info-area">
+//                         <div class="product-brand-description">
+//                             <span class="brand">${product.brand}</span>
+//                             <span class="name">${product.title}</span>
+//                         </div>
+//                         <div class="rating-container">
+//                             <div class="rating-score">${product.rating}</div>
+//                             <div class="ratings" data-productid=${product.id}>
+//                                 <div class="star">
+//                                     <i class="fa-regular fa-star"></i>
+//                                 </div>
+//                                 <div class="star">
+//                                     <i class="fa-regular fa-star"></i>
+//                                 </div>
+//                                 <div class="star">
+//                                     <i class="fa-regular fa-star"></i>
+//                                 </div>
+//                                 <div class="star">
+//                                     <i class="fa-regular fa-star"></i>
+//                                 </div>
+//                                 <div class="star">
+//                                     <i class="fa-regular fa-star"></i>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                         <div class="price-wrapper">
+//                             ${product.discountPercentage ? `
+//                             <b class="badge-old-price">$${(product.price / (1 - product.discountPercentage / 100)).toFixed(2)}</b>
+//                             `:""}
+                            
+//                             <b class="badge-new-price">$${product.price}</b>
+//                         </div>
+//                     </div>
+//                 </a>
+//             </div>
+//             `;
+//             ratingProduct(product.rating,product.id);
+//     }
+// }
+// listProducts();
+
+function ratingProduct(rating,productId){
+    const ratingStar = document.querySelectorAll(`.ratings[data-productid="${productId}"] .star`);
+    for (let i = 0; i < Math.ceil(rating) ; i++) {
+        ratingStar[i].innerHTML = `<i class="fa-solid fa-star"></i>`
+    }
+}
+
+async function saveProductsToLocalStorage(){
+    const items = await fetchDummyJson('products?limit=100');
+    const productsLength = items.products
+    localStorage.setItem('products', JSON.stringify(productsLength));
+}
+
+
+async function loadMoreProducts() {
+    const items = await fetchDummyJson(`products?limit=9&skip=${skip}`);
     const products = items.products;
-    localStorage.setItem('products',JSON.stringify(items.products));
-    for (const product of products) {
-        productPage.innerHTML += 
+    if (products.length > 0) {
+        for (const product of products) {
+            productPage.innerHTML +=
             `
             <div class="product-col-3">
                 <a href="product-page.html?id=${product.id}" class="product" data-productid=${product.id}>
@@ -56,15 +134,12 @@ async function listProducts(){
                 </a>
             </div>
             `;
-            ratingProduct(product.rating,product.id);
+            ratingProduct(product.rating, product.id);
+        }
+        skip += products.length;
+        loading = false;
     }
-}
+  }
 
-function ratingProduct(rating,productId){
-    const ratingStar = document.querySelectorAll(`.ratings[data-productid="${productId}"] .star`);
-    for (let i = 0; i < Math.ceil(rating) ; i++) {
-        ratingStar[i].innerHTML = `<i class="fa-solid fa-star"></i>`
-    }
-}
-
-listProducts();
+  saveProductsToLocalStorage()
+  loadMoreProducts()
